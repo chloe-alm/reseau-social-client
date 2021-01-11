a69d5001fdd3d3a5298648c488a12258c644390e
MacBook-Pro-de-Chloe <cdargelez@gmail.com>
2021-01-11 03:21:27 +0100

    deleteEvent ok

--- src/components/molecules/cardEventAdmin.jsx
+++ src/components/molecules/cardEventAdmin.jsx
@@ -29,13 +29,58 @@
 //     </div>
 //   );
 // }
-import React from "react";
-import { Link } from "react-router-dom";
+import Axios from "axios";
+import React, { useContext, useState } from "react";
+import { useAlert } from "react-alert";
+import { Link, useHistory } from "react-router-dom";
+import { AuthContext } from "../../context/auth";
 import NavBar from "./NavBar";
 require("../molecules/_cardEvent.scss");
 
 export default function CardEvent({ event }) {
-  console.log("EVENTTT",event)
+  const token = localStorage.getItem("token");
+  const { state } = useContext(AuthContext);
+  const history = useHistory();
+  const alert = useAlert();
+  const [deleteEvent, setDeleteEvent] = useState({
+    title:event.title,
+    content: event.content,
+    hashtag: event.hashtag,
+    isSubmitting: false,
+    errorMessage: null,
+  });
+  console.log("state1",state)
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setDeleteEvent({
+        ...deleteEvent,
+        isSubmitting: true,
+      });
+      const result = await Axios({
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        url: `http://localhost:8001/api/events/${event.id}`,
+        data: JSON.stringify(deleteEvent),
+      });
+      console.log(result);
+      if (result.status === 201) {
+        return alert.show("Event bien supprimé"),history.push("/");
+      }
+    } catch (error) {
+      setDeleteEvent({
+        ...deleteEvent,
+        isSubmitting: false,
+        errorMessage: error.response.data.description,
+      });
+    }
+  };
+
+
+
   return (
     <div>
       
@@ -52,6 +97,24 @@ export default function CardEvent({ event }) {
           </p>
         </Link>
       </div>
+      {
+            
+            state.user.isAdmin === true ? (
+              <div>
+                <div className="container_OnePost_erreur">
+            {deleteEvent.errorMessage}
+          </div>
+              <button
+              type="submit"
+              className="container_OnePost_boutonSupprimer"
+              onClick={handleSubmit}
+            >
+              Supprimer
+            </button>
+            </div>
+            ):(<div></div>)
+            
+            }
     </div>
   );
 }
